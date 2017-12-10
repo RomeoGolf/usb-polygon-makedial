ecc000a0de7016f740c55fc01e0a4e65f2c0ee8f
RomeoGolf <RomeoGolf@local>
2017-12-10 17:58:23 +0500

    The function for adding command bytes to data array

--- makeDial.cpp
+++ makeDial.cpp
@@ -10,9 +10,36 @@
 #define ENTRY_NUM 20
 #define DATA_SIZE (ENTRY_SIZE * ENTRY_NUM)
 
+void addCoord(uint8_t * data, int entry) {
+    int index;
+
+    index = (entry * ENTRY_SIZE);
+    data[index++] = 0x01;
+    data[index++] = 0x42;
+    data[index++] = 0x01;
+    data[index++] = 0xA6;
+    data[index++] = 0x02;
+    index = (entry * ENTRY_SIZE) + LINE_SIZE;
+    data[index++] = 0x01;
+    data[index++] = 0x43;
+    data[index++] = 0x01;
+    data[index++] = 0xA6;
+    data[index++] = 0x02;
+    index = (entry * ENTRY_SIZE) + (LINE_SIZE * 2);
+    data[index++] = 0x01;
+    data[index++] = 0x44;
+    data[index++] = 0x01;
+    data[index++] = 0xA6;
+    data[index++] = 0x02;
+}
+
 int main() {
     uint8_t data[DATA_SIZE];
 	memset(data, 0, DATA_SIZE);
+
+    for(int i = 0; i < 20; i++) {
+        addCoord(data, i);
+    }
 
     std::string dfName = "DataFile.bin";
     std::ofstream dataFile {dfName, std::ios::binary | std::ios::trunc};
